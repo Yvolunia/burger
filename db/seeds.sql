31649d18587d33b14cc63eeb0562237e241c10e3
Yvonne Reichenbach <yvonnereichenbach@Yvonnes-MacBook-Pro.local>
2018-03-10 00:26:19 -0500

    Add SQL files

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("hamburger");
+
+
+INSERT INTO burgers (burger_name)
+VALUES ("double cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Greek lamb burger");
---
+++ server.js
@@ -0,0 +1,10 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOveride = require("method-override");
