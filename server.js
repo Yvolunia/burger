5ea6c0f227fee908a1821036d22d6b41e314313e
Yvonne Reichenbach <yvonnereichenbach@Yvonnes-MacBook-Pro.local>
2018-04-28 00:26:57 -0400

    Style Fix

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOveride = require("method-override");
 
 var PORT = process.env.PORT || 3000;
-
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
